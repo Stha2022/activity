b4d155888c51d8ddc882eb08061302da28d9d631
Stha2022 <ngwenyasithandweyinkosi613@gmail.com>
2024-02-07 14:03:57 +0200

    my first two functions

---
+++ activity.py
@@ -0,0 +1,16 @@
+#1st Function
+def my_function():
+  name = input("enter your name: ")
+  return name
+
+name = my_function()
+
+#2nd function
+def interger():
+  age = int(input("enter your age: "))
+  return age
+
+
+
+
+
